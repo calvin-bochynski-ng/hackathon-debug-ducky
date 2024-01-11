7d7ef021d6e3867f4b18655a030067b75fcd6f7e
Calvin <calvinng1993@gmail.com>
2024-01-11 09:59:10 +0000

    Setting up CORS and .env

--- server/server.js
+++ server/server.js
@@ -1,2 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const SERVER_PORT = process.env.PORT;
+const SERVER_API_URL = process.env.API_URL;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(
+  SERVER_PORT,
+  console.log(`Server has been started at ${SERVER_API_URL}:${SERVER_PORT}`)
+);
